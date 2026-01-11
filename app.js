ac0535bb04c3227bcba179c9c96b9a496ee9ea96
HomairaHoma1122 <homaira.amini22@gmial.com>
2026-01-11 10:35:50 -0800

    add some changes to pro

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
-const bt = document.getElementById("bt").value;
-const adding = document.getElementById("order").value;
-const input = document.getElementById("todo").value;
+const btn = document.getElementById("addId").value;
+const eror = document.getElementById("eror");
 const button = document.getElementById("addbtn");
-const mainfoem = document.getElementById("mainDiv");
+const mainform = document.getElementById("mainDiv");
 
 const todolist = [];
-bt.addEventListener("click", ()={
-    if(mainform.classlist.contains("hidden")){
-        mainform.classlist.remove("hidden");
+let peIm = "";
+
+mainform.addEventListener("click", ()=>{
+    if(mainform.classlist.contain("none")){
+    mainform.classlist.remove("none");
     mainform.classlist.add("flex");
     }
-    else{mainform.classlist.remove("flex");
-        mainform.classlist.add("hidden");
-
+    else {
+        mainform.classlist.remove("flex");
+        mainform.classList.add("none");
     }
-})
+});
 
+button.addEventListener("click",(ev)=>{
+        const h1 = document.createElement("h1");
+ev.preventDefault();
+  saveTodo();
+  h1.innerHTML = "write list";
+
+if(peIm.length>0){
+
+    h1.textContent = "Please write a list and try agien";
+    eror.appendChild(h1);
+}
+});
+
+function saveTodo(){
+    eror.innerHTML = "";
+    const h1 = document.createElement("h1");
+    const todo = document.getElementById("order").value;
+    const adding = document.getElementById("todo").value;
+    if(adding !==""){
+
+    
+    const saveTodo = {
+        myTOdo: adding,
+        import: todo,
+    }
+    
+    todolist.push(saveTodo);
+}
+else{
+    peIm = "Please write a list and try agien";
+}
+}
+
+//
+if(peIm.length>0){
+    const h1 = document.createElement("h1");
+    h1.textContent = "Please write a list and try agien";
+}
